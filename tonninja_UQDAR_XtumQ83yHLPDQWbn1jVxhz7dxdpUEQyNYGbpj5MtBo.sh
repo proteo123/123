869f8012d6e4e02f35907af0f8de5afda23b914e
proteo123 <159125862+proteo123@users.noreply.github.com>
2024-02-06 16:31:52 +0700

    Add files via upload

---
+++ tonninja_UQDAR_XtumQ83yHLPDQWbn1jVxhz7dxdpUEQyNYGbpj5MtBo.sh
@@ -0,0 +1,8 @@
+
+    #!/bin/bash
+
+    while true; do
+      ./miningPoolCli -pool-id=UQDAR_XtumQ83yHLPDQWbn1jVxhz7dxdpUEQyNYGbpj5MtBo -url https://ninja.tonlens.com
+      sleep 1;
+    done;
+          
